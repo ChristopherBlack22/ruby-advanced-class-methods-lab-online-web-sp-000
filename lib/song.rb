4ddfd25cd7dbcb2f56c8884081e7ad7be0750db3
Christopher Black <christopher.black.2209@googlemail.com>
2021-04-19 23:24:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,8 +55,8 @@ class Song
   end 
 
   def self.create_from_filename(filename)
-    new_from_filename(filename)
-    save
+    song = new_from_filename(filename)
+    song.save
   end 
   
   def self.destroy_all
